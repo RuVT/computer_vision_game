b70d72244d37e2d73fd0c37bc2391c801c769297
RuVT <vazquezt101@gmail.com>
2016-04-26 12:00:55 -0700

    Try to find blobs

--- kivy_ui/mainApp.py
+++ kivy_ui/mainApp.py
@@ -11,9 +11,20 @@ import cv2
 
 from pygame.image import tostring
 
+def getKivyTexture(img):
+	buffer = tostring(img.getPGSurface(),'RGB',True)
+	# buffer = img.toString()
+	imdata = ImageData(img.width, img.height, 'rgb', buffer)
+	tex = Texture.create_from_data(imdata)
+	return tex
+
+
 class ControlUI(Widget):
-	# val = ListProperty([])
+	val = ListProperty([])
 	cvImage = ObjectProperty();
+	cvFilter = ObjectProperty();
+	cvActiveBloops = ObjectProperty();
+	blop_coord = ObjectProperty();
 	# showBinarie = BooleanProperty(False)
 	ts = []
 	bb = (100,100,100,100,)
@@ -22,36 +33,23 @@ class ControlUI(Widget):
 	def update(self, dt):
 		# if self.display.isNotDone():
 		self.img = self.cam.getImage().flipHorizontal()
-		buffer = tostring(self.img.getPGSurface(),'RGB',True)
-		imdata = ImageData(self.img.width, self.img.height, 'rgb', buffer)
-		tex = Texture.create_from_data(imdata)
-		self.cvImage.texture = tex
-		# down = self.display.leftButtonDownPosition()
-		# up = self.display.leftButtonUpPosition()
-		# if(up is not None and down is not None):
-		# 	bb = self.display.pointsToBoundingBox(up, down)
-		# 	print(bb)
-		# 	self.img.drawRectangle(bb[0],bb[1],bb[2],bb[3])
-		# lower = np.array((float(self.val[0]), float(self.val[1]), float(self.val[2])))
-		# upper = np.array((float(self.val[3]), float(self.val[4]), float(self.val[5])))
-		# self.ts = self.img.track(img=self.img, ts=self.ts, bb=self.bb)
-		# if self.ts:
-		# 	self.ts[-1].draw()
-		# 	self.ts[-1].drawBB()
-		# lower = np.array((64.,129.,159.))
-		# upper = np.array((80., 250., 245.))
-		# hsv = self.img.toHSV()
-		# mask = cv2.inRange(hsv.getNumpy(), lower, upper)
-		# img2 = Image(source=mask)
-		# img2 = img2.erode(1)
-		# img2 = img2.dilate(2)
-		# blops = img2.findBlobs(minsize=50, maxsize=100,)
-		# if blops:
-		# 	for b in blops:
-		# 		if b.isRectangle() and b.minRectWidth():
-		# 			self.img.draw(b)
-		# self.img.save(self.display)
+		self.img.drawRectangle(50,50,200,200)
+		lower = np.array((float(self.val[0]), float(self.val[1]), float(self.val[2])))
+		upper = np.array((float(self.val[3]), float(self.val[4]), float(self.val[5])))
 
+		hsv = self.img.toHSV()
+		mask = cv2.inRange(hsv.getNumpy(), lower, upper)
+		img2 = Image(source=mask)
+		img2 = img2.erode(1)
+		img2 = img2.dilate(2)
+		if self.cvActiveBloops.active:
+			blops = img2.findBlobs(minsize=50, maxsize=200)
+			if blops:
+				for b in blops:
+					if b.isRectangle():
+						self.blop_coord.text = b.centroid()
+		self.cvFilter.texture = getKivyTexture(img2)
+		self.cvImage.texture = getKivyTexture(self.img)
 
 
 class MainApp(App):
